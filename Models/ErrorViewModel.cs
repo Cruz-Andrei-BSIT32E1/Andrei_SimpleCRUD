82e73a60ca15c340a96debf311a20ec4e0629f9f
GerodiazRogel-BSIT32E1 <161409141+Cruz-Andrei-BSIT32E1@users.noreply.github.com>
2024-09-04 16:33:36 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Andrei_SimpleCRUD.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
