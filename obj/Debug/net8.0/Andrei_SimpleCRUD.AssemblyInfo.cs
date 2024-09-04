c0628f4bebcb5f3c039b0b4cfd0b10049f7cd4cd
GerodiazRogel-BSIT32E1 <161409141+Cruz-Andrei-BSIT32E1@users.noreply.github.com>
2024-09-04 16:48:03 +0800

    Creating folder and files for entities in StudentSectionApp.Domain

--- obj/Debug/net8.0/Andrei_SimpleCRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/Andrei_SimpleCRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Andrei_SimpleCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f286ab79ff7b8a96d6db069a93bf360b794656b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f16fb3cdfce064e681ab3a194667fa397303232")]
 [assembly: System.Reflection.AssemblyProductAttribute("Andrei_SimpleCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Andrei_SimpleCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
