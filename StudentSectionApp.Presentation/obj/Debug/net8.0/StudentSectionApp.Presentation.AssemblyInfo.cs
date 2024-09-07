70ca0b5a8c366a010023da5363b0413257ce6f41
GerodiazRogel-BSIT32E1 <161409141+Cruz-Andrei-BSIT32E1@users.noreply.github.com>
2024-09-07 23:36:40 +0800

    creating views file for student and sectoion

--- StudentSectionApp.Presentation/obj/Debug/net8.0/StudentSectionApp.Presentation.AssemblyInfo.cs
+++ StudentSectionApp.Presentation/obj/Debug/net8.0/StudentSectionApp.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentSectionApp.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a0c7299a8d8bea67c8d4f8494ae535194dc0f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e357e76b994cd3b6988d1d819790c9dffcb8f458")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentSectionApp.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentSectionApp.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
